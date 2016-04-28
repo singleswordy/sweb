d23894236c82e59117c7aeae9fbe9cd8d63ba48c
singleswordy <singlesword@126.com>
2016-04-28 23:12:43 +0800

    delete

--- static/js/common.js
+++
@@ -1,9 +0,0 @@
-/**
- * 异步加载图片
- * $imgContainer 需要具有 imgsrc,imgstyle 2个属性
- */
-function loadImg($imgContainer){
-    var imgUrl = $imgContainer.attr('imgsrc'),
-        imgStyle = {'style':$imgContainer.attr('imgstyle')};
-    $imgContainer.get(0).innerHTML = ReferrerKiller.imageHtml(imgUrl,imgStyle);
-}
